cb68ea75f77d3dd88133c7691e69cbf764a582dd
Yuki Morishita <mor.yuki@gmail.com>
2011-02-14 08:55:28 +0900

    modified to use node thrift

--- cassandra.js
+++ cassandra.js
@@ -1,23 +1,99 @@
-var thrift = require('thrift'),
+// copyright 2011 Yuki Morishita<mor.yuki@gmail.com>
+
+var sys = require('sys'),
+    thrift = require('thrift'),
     Cassandra = require('./gen-nodejs/Cassandra'),
     ttype = require('./gen-nodejs/cassandra_types');
 
+/**
+ * Cassandra Client
+ *
+ * @constructor
+ * @api public
+ */
 var Client = function(host) {
   var pair = host.split(/:/);
-  var connection = thrift.createConnection(pair[0], pair[1]);
-
-  connection.on('error', function(err) {
-    // do handle connection err
-  });
-
-  this.client_ = thrift.createClient(Cassandra, connection);
-
+  this.host = pair[0];
+  this.port = pair[1];
   this.defaultCL = {
     read: ttype.ConsistencyLevel.QUORUM,
     write: ttype.ConsistencyLevel.QUORUM
   };
+
+  this.state = 0;
 }
 
+sys.inherits(Client, process.EventEmitter);
+
+/**
+ * Connect to cassandra.
+ *
+ * @param credential if given, try logging in to cassandra
+ * @param callback called when connected and client is ready
+ */
+
+Client.prototype.connect = function(keyspace, credential) {
+
+  this.keyspace = keyspace;
+  this.connected = false;
+
+  var args = Array.prototype.slice.call(arguments);
+
+  this.connection = thrift.createConnection(this.host, this.port);
+  this.connection.on('error', function(err) {
+    // TODO handle connection err
+    console.dir('error:' + err);
+  });
+  this.connection.on('connect', function(err) {
+    // TODO handle connection err
+  });
+
+  this.thrift_client = thrift.createClient(Cassandra, this.connection);
+
+  var self = this;
+  this.thrift_client.describe_keyspace(this.keyspace, function(err, ksdef) {
+    if (err) {
+      throw new Error(err);
+    }
+
+    self.definition_ = ksdef;
+    self.column_families_ = {};
+
+    var i = ksdef.cf_defs.length;
+    var cf;
+    while (i--) {
+      cf = ksdef.cf_defs[i];
+      self.column_families_[cf.name] = cf;
+    }
+
+    self.thrift_client.set_keyspace(self.keyspace, function(err) {
+      if (err) {
+        throw new Error(err.message);
+      }
+
+      // if credential is given, then call login
+      if (credential) {
+        self.thrift_client.login(
+          new ttype.AuthenticationRequest(credential), function(err) {
+          
+          if (err) {
+            // TODO handle err
+            throw new Error(err);
+          }
+
+          // only when login is success, emit connected event
+          self.emit('connected', self.column_families_);
+        });
+      } else {
+        self.emit('connected', self.column_families_);
+      }
+    });
+  });
+}
+
+/**
+ * Set or get default consistency level
+ */
 Client.prototype.consistencyLevel = function() {
   if (arguments.length == 0) {
     return this.defaultCL;
@@ -28,44 +104,432 @@ Client.prototype.consistencyLevel = function() {
   }
 }
 
-Client.prototype.cf = function(name) {
-  return new ColumnFamily(this.client_, name);
-}
+/**
+ * Get column family to perform query or mutation
+ */
+Client.prototype.getColumnFamily = function(name) {
+  return new ColumnFamily(this, name);
+};
 
+/**
+ * Close connection
+ */
+Client.prototype.close = function() {
+  this.connection.end();
+};
+
+/**
+ *
+ * @param client Client
+ * @param name name of this Column Family
+ * @constructor
+ */
 var ColumnFamily = function(client, name) {
-  this.client_ = client;
   this.name = name;
-}
+  this.queue = [];
+  this.ready = false;
+  this.client_ = client;
 
-Client.prototype.get = function() {
-  var args = Array.prototype.slice(arguments);
+  var self = this;
+  this.client_.on('connected', function(cfdef) {
+    // check to see if column name is valid
+    var cf = cfdef[self.name];
+    if (!cf) {
+      // column family does not exist
+      throw new Error('Column Family ' + self.name + ' does not exist.');
+    }
+
+    // copy all cfdef properties
+    for (var prop in cf) {
+      if (cf.hasOwnProperty(prop)) {
+        self[prop] = cf[prop];
+      }
+    }
+    self.isSuper = self.column_type === 'Super';
+    self.ready = true;
+
+    self.dispatch();
+  });
+};
+
+/**
+ * Get data from cassandra
+ *
+ * @param keys row keys to fetch
+ * @param columns optional. which columns to retrieve
+ * @param options optional. valid params are start, finish, reversed, count
+ * @param callback callback function which called after data retrieval.
+ */
+ColumnFamily.prototype.get = function() {
+  var args = Array.prototype.slice.call(arguments);
+  if (!this.ready) {
+    this.queue.push([arguments.callee, args]);
+    return;
+  }
+
+  // last argument may be callback
   var callback;
   if (typeof args[args.length - 1] === 'function') {
     callback = args.pop();
   }
+  // keys to get
   var keys = args.shift();
-  var columns = args.shift() || {};
-  var column_parent = new ttype.ColumnParent({
-    column_family: this.name
-  });
-  if (columns) {
-    if (typeof columns == "object") {
-      // super columns
+  // if only one key specified, turn it to array
+  if (!(keys instanceof Array)) {
+    keys = [keys];
+  }
+
+  var method_args = this.isSuper ? this.parseArgumentsForSuperCF_(args) : this.parseArgumentsForStandardCF_(args);
+  var column_parent = method_args[0];
+  var predicate = method_args[1];
+  var cl = method_args[2] || this.client_.defaultCL.read;
+
+  var self = this;
+  this.client_.thrift_client.multiget_slice(
+      keys, column_parent, predicate, cl,
+      function(err, res) {
+        if (err) {
+          callback(err, obj);
+        }
+
+        var obj = {};
+        var key, col, sub_col;
+        for (key in res) {
+          if (res.hasOwnProperty(key)) {
+            obj[key] = {};
+            var i = res[key].length;
+            while (i--) {
+              col = res[key][i].super_column;
+              if (col) {
+                // super
+                obj[key][col.name] = {};
+                var j = col.columns.length;
+                while (j--) {
+                  sub_col = col.columns[j];
+                  obj[key][col.name][sub_col.name] = sub_col.value;
+                }
+              } else {
+                // standard
+                col = res[key][i].column;
+                obj[key][col.name] = col.value;
+              }
+            }
+          }
+        }
+        if (keys.length == 1) {
+          obj = obj[keys[0]];
+        }
+        callback(err, obj);
+      });
+};
+
+/**
+ * Get column count from cassandra
+ *
+ * @param keys row keys to fetch
+ * @param columns optional. which columns to retrieve
+ * @param options optional. valid params are start, finish, reversed, count
+ * @param callback callback function which called after data retrieval.
+ */
+ColumnFamily.prototype.count = function() {
+  var args = Array.prototype.slice.call(arguments);
+  if (!this.ready) {
+    this.queue.push([arguments.callee, args]);
+    return;
+  }
+
+  // last argument may be callback
+  var callback;
+  if (typeof args[args.length - 1] === 'function') {
+    callback = args.pop();
+  }
+  // keys to get
+  var keys = args.shift();
+  // if only one key specified, turn it to array
+  if (!(keys instanceof Array)) {
+    keys = [keys];
+  }
+
+  var method_args = this.isSuper ? this.parseArgumentsForSuperCF_(args) : this.parseArgumentsForStandardCF_(args);
+  var column_parent = method_args[0];
+  var predicate = method_args[1];
+  var cl = method_args[2] || this.client_.defaultCL.read;
+
+  this.client_.thrift_client.multiget_count(
+      keys, column_parent, predicate, cl,
+      function(err, res) {
+        if (err) {
+          callback(err, obj);
+        }
+        
+        var obj = {};
+        var key, count;
+        for (key in res) {
+          if (res.hasOwnProperty(key)) {
+            obj[key] = res[key];
+          }
+        }
+        if (keys.length == 1) {
+          obj = obj[keys[0]];
+        }
+        callback(err, obj);
+      });
+};
+
+
+/**
+ * set (insert or update) data
+ */
+ColumnFamily.prototype.set = function() {
+  var args = Array.prototype.slice.call(arguments);
+  if (!this.ready) {
+    this.queue.push([arguments.callee, args]);
+    return;
+  }
+
+  var callback;
+  if (typeof args[args.length - 1] === 'function') {
+    callback = args.pop();
+  }
+
+  var key = args.shift();
+  var values = args.shift() || {};
+  var ts = new Date().getTime();
+
+  var prop, value;
+  var mutations = [], columns;
+  if (this.isSuper) {
+    // super
+    for (prop in values) {
+      if (values.hasOwnProperty(prop)) {
+        columns = [];
+        value = values[prop];
+        for (var col in value) {
+          columns.push(new ttype.Column({
+            name: col,
+            value: '' + value[col],
+            timestamp: ts,
+            ttl: null
+          }));
+        }
+        // prop is super column name
+        mutations.push(new ttype.Mutation({
+          column_or_supercolumn: new ttype.ColumnOrSuperColumn({
+            super_column: new ttype.SuperColumn({
+              name: prop,
+              columns: columns
+            })
+          })
+        }));
+      }
+    }
+  } else {
+    // standard
+    for (prop in values) {
+      mutations.push(new ttype.Mutation({
+        column_or_supercolumn: new ttype.ColumnOrSuperColumn({
+          column: new ttype.Column({
+            name: prop,
+            value: '' + values[prop],
+            timestamp: ts,
+            ttl: null
+          })
+        })
+      }));
     }
   }
-  // CassandraClient.prototype.multiget_slice = 
-  //   function(
-  //     keys,
-  //     column_parent,
-  //     predicate,
-  //     consistency_level,callback)
-  this.multiget_slice(
-      keys,
-      column_parent,
-      null,
 
-      callback);
+  var mutation_map = {};
+  mutation_map[key] = {};
+  mutation_map[key][this.name] = mutations;
+
+  this.client_.thrift_client.batch_mutate(
+      mutation_map, ttype.ConsistencyLevel.ONE, callback);
+};
+
+/**
+ * remove data
+ */
+ColumnFamily.prototype.remove = function() {
+  var args = Array.prototype.slice.call(arguments);
+  if (!this.ready) {
+    this.queue.push([arguments.callee, args]);
+    return;
+  }
+
+  var callback;
+  if (typeof args[args.length - 1] === 'function') {
+    callback = args.pop();
+  }
+
+  var key = args.shift();
+
+  var method_args = this.isSuper ? this.parseArgumentsForSuperCF_(args) : this.parseArgumentsForStandardCF_(args);
+  var column_parent = method_args[0];
+  var predicate = method_args[1];
+  var cl = method_args[2] || this.client_.defaultCL.write;
+
+  var ts = new Date().getTime();
+
+  var mutations = [];
+  mutations.push(new ttype.Mutation({
+    deletion: new ttype.Deletion({
+      timestamp: ts,
+      super_column: column_parent.super_column,
+      predicate: predicate.column_names ? predicate : null
+    })
+  }));
+
+  var mutation_map = {};
+  mutation_map[key] = {};
+  mutation_map[key][this.name] = mutations;
+
+  this.client_.thrift_client.batch_mutate(mutation_map, cl, callback);
+};
+
+/**
+ * dispatch queries when client is ready
+ * @api private
+ **/
+ColumnFamily.prototype.dispatch = function() {
+  if (this.ready) {
+    if (this.queue.length > 0) {
+      var next = this.queue.shift();
+      next[0].apply(this, next[1]);
+
+      this.dispatch();
+    }
+  } else {
+    this.dispatch();
+  }
+};
+
+/**
+ * 
+ * @api private
+ * @param args
+ * @return [ColumnParent, SlicePredicate, ConsistencyLevel]
+ */
+ColumnFamily.prototype.parseArgumentsForSuperCF_ = function(args) {
+  var default_options = {
+    start: '',
+    finish: '',
+    reversed: false,
+    count: 100,
+    consistencyLevel: null
+  };
+  var column_parent = {
+    column_family: this.name
+  };
+  var predicate = {};
+
+  var super_column_or_options = args.shift();
+  var options = default_options;
+
+  if (super_column_or_options) {
+    // first argumet may be super column name
+    if (super_column_or_options instanceof String ||
+        typeof super_column_or_options === 'string') {
+      column_parent.super_column = super_column_or_options;
+      var columns_or_options = args.shift();
+      if (columns_or_options) {
+        var columns, options, option_name;
+        if (typeof columns_or_options.slice === 'function') {
+          // first argument is column name(s)
+          columns = columns_or_options.slice();
+          if (!(columns instanceof Array)) {
+            columns = [columns];
+          }
+          predicate.column_names = columns;
+          options = args.shift() || default_options;
+        } else {
+          // update default option with given value
+          for (option_name in columns_or_options) {
+            if (columns_or_options.hasOwnProperty(option_name)) {
+              options[option_name] = columns_or_options[option_name];
+            }
+          }
+          predicate.slice_range = new ttype.SliceRange(options);
+        }
+      } else {
+        predicate.slice_range = new ttype.SliceRange(options);
+      }
+    } else {
+      // update default option with given value
+      for (option_name in super_column_or_options) {
+        if (super_column_or_options.hasOwnProperty(option_name)) {
+          options[option_name] = super_column_or_options[option_name];
+        }
+      }
+      predicate.slice_range = new ttype.SliceRange(options);
+    }
+  } else {
+    options = default_options;
+    // update default option with given value
+    for (option_name in super_column_or_options) {
+      if (columns_or_options.hasOwnProperty(option_name)) {
+        options[option_name] = columns_or_options[option_name];
+      }
+    }
+    predicate.slice_range = new ttype.SliceRange(options);
+  }
+
+  return [new ttype.ColumnParent(column_parent),
+         new ttype.SlicePredicate(predicate),
+         options.consistencyLevel];
 }
+
+/**
+ *
+ * @api private
+ * @param args
+ * @return [ColumnParent, SlicePredicate, ConsistencyLevel]
+ */
+ColumnFamily.prototype.parseArgumentsForStandardCF_ = function(args) {
+  var default_options = {
+    start: '',
+    finish: '',
+    reversed: false,
+    count: 100,
+    consistencyLevel: null
+  };
+  var column_parent = {
+    column_family: this.name
+  };
+  var predicate = {};
+
+  var columns_or_options = args.shift();
+  var options = default_options;
+
+  if (columns_or_options) {
+    var columns, options, option_name;
+    if (typeof columns_or_options.slice === 'function') {
+      // first argument is column name(s)
+      columns = columns_or_options.slice();
+      if (!(columns instanceof Array)) {
+        columns = [columns];
+      }
+      predicate.column_names = columns;
+      options = args.shift() || default_options;
+    } else {
+      // update default option with given value
+      for (option_name in columns_or_options) {
+        if (columns_or_options.hasOwnProperty(option_name)) {
+          options[option_name] = columns_or_options[option_name];
+        }
+      }
+      predicate.slice_range = new ttype.SliceRange(options);
+    }
+  } else {
+    predicate.slice_range = new ttype.SliceRange(options);
+  }
+
+  return [new ttype.ColumnParent(column_parent),
+         new ttype.SlicePredicate(predicate),
+         options.consistencyLevel];
+}
+
+/** module exports */
 
 exports.Client = Client;
 exports.ConsistencyLevel = ttype.ConsistencyLevel;
--- test/test.js
+++ test/test.js
@@ -6,208 +6,276 @@
  *
  * cassandra-cli --host localhost --batch < test.ddl
  */
-var assert = require("assert"),
-    cassandra = require("../build/default/cassandra");
+var assert = require('assert'),
+    cassandra = require('../cassandra');
+
+// number of tests
 
 // connect to cassandra
-var client = new cassandra.Client("node_cassandra_test", "127.0.0.1:9160");
+var client = new cassandra.Client('127.0.0.1:9160');
 
-// login if needed
-// client.login("user", "pass");
+module.exports = {
 
-// make sure all consistency levels are exported
-var CL = cassandra.ConsistencyLevel;
-assert.deepEqual(CL, {
-  ONE: 1,
-  QUORUM: 2,
-  LOCAL_QUORUM: 3,
-  EACH_QUORUM: 4,
-  ALL: 5,
-  ANY: 6
-});
+  'test if ConsistencyLevel is exported properly': function() {
+    // make sure all consistency levels are exported
+    var CL = cassandra.ConsistencyLevel;
+    assert.deepEqual(CL, {
+      ONE: 1,
+      QUORUM: 2,
+      LOCAL_QUORUM: 3,
+      EACH_QUORUM: 4,
+      ALL: 5,
+      ANY: 6
+    });
 
-// client configuration
-// consistency level
-// default is CL.QUORUM for both reads and writes
-assert.deepEqual(client.consistencyLevel(), {
-  write: CL.QUORUM,
-  read: CL.QUORUM
-});
-// let's change default
-client.consistencyLevel({
-  write: CL.ONE,
-  read: CL.ONE
-});
-// and check
-assert.deepEqual(client.consistencyLevel(), {
-  write: CL.ONE,
-  read: CL.ONE
-});
-
-//-------------------------------------
-// insert
-// insert one record to standard column family.
-client.insert("Standard", "todd", {
-  id: 1,
-  first_name: "Todd",
-  last_name: "Dahl",
-  age: 24
-});
-
-// make sure it is inserted.
-var get_result = client.get("Standard", "todd");
-assert.deepEqual(get_result, {
-  id: "1",
-  first_name: "Todd",
-  last_name: "Dahl",
-  age: "24",
-});
-// note that even though you inserted Number,
-// cassandra returns in String.
-
-// if you query for the key that doesn't exist, you will get empty object.
-var empty = client.get("Standard", "notexist");
-assert.deepEqual(empty, {});
-
-// see if multiget works as expected.
-client.insert("Standard", "jesse", {
-  id: 2,
-  first_name: "Jesse",
-  last_name: "Pitman"
-});
-
-var multiget_result = client.multiget("Standard", ["todd", "jesse"]);
-assert.deepEqual(multiget_result, {
-  todd: {
-    id: "1",
-    first_name: "Todd",
-    last_name: "Dahl",
-    age: "24",
+    // client configuration
+    // consistency level
+    // default is CL.QUORUM for both reads and writes
+    assert.deepEqual(client.consistencyLevel(), {
+      write: CL.QUORUM,
+      read: CL.QUORUM
+    });
+    // let's change default
+    client.consistencyLevel({
+      write: CL.ONE,
+      read: CL.ONE
+    });
+    // and check
+    assert.deepEqual(client.consistencyLevel(), {
+      write: CL.ONE,
+      read: CL.ONE
+    });
   },
-  jesse: {
-    id: "2",
-    first_name: "Jesse",
-    last_name: "Pitman"
-  }
-});
 
-// read operation with options.
-// valid options are:
-//   start: SliceRange start
-//   finish: SliceRange finish
-//   reversed: SliceRange reversed
-//   limit: SliceRange count
-//
-//   ttl: column ttl (not yet)
-//   consistency_level: read consisteny level (not yet)
-//
-// specifying column names
-var selected_result = client.get("Standard", "todd", ['id', 'age']);
-assert.deepEqual(selected_result, {
-  id: "1",
-  age: "24",
-});
-// specifying column names and CL
-var selected_result_with_cl_any = client.get("Standard", "todd",
+  'test if operations on client works properly': function(beforeExit) {
+    client.connect('node_cassandra_test');
+    // or login if needed
+    //client.connect('node_cassandra_test', {username: 'foo', password: 'bar'});
+
+    var standard, superCF, notExistCF;
+    standard = client.getColumnFamily('Standard');
+    superCF = client.getColumnFamily('Super');
+    //notExistCF = client.getColumnFamily('NotExistCF');
+
+    //-------------------------------------
+    // set
+    // set one record to standard column family.
+    standard.set('todd', {
+      id: 1,
+      first_name: 'Todd',
+      last_name: 'Dahl',
+      age: 24
+    }, function(err) {
+      assert.equal(err, null);
+    });
+
+    // make sure it is seted.
+    standard.get('todd', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        id: '1',
+        first_name: 'Todd',
+        last_name: 'Dahl',
+        age: '24',
+      });
+    });
+    // note that even though you set Number,
+    // cassandra returns in String.
+
+    // if you query for the key that doesn't exist, you will get empty object.
+    standard.get('notexist', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {});
+    });
+    /**
+      notExistCF.get('notexist', function(err, res) {
+      console.dir(err);
+      assert.equal(res, null);
+      });
+      */
+
+    // see if multiget works as expected.
+    standard.set('jesse', {
+      id: 2,
+      first_name: 'Jesse',
+      last_name: 'Pitman'
+    });
+
+    standard.get(['todd', 'jesse'], function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        todd: {
+                id: '1',
+        first_name: 'Todd',
+        last_name: 'Dahl',
+        age: '24',
+              },
+        jesse: {
+                 id: '2',
+        first_name: 'Jesse',
+        last_name: 'Pitman'
+               }
+      });
+    });
+
+    // read operation with options.
+    // valid options are:
+    //   start: SliceRange start
+    //   finish: SliceRange finish
+    //   reversed: SliceRange reversed
+    //   count: SliceRange count
+    //
+    //   ttl: column ttl (not yet)
+    //   consistency_level: read consisteny level (not yet)
+    //
+    // specifying column names
+    standard.get('todd', ['id', 'age'], function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        id: '1',
+        age: '24',
+      });
+    });
+    /*
+    // specifying column names and CL
+    var selected_result_with_cl_any = client.get('Standard', 'todd',
     ['id', 'age'],
     {consistency_level: CL.ANY});
-assert.deepEqual(selected_result_with_cl_any, {
-  id: "1",
-  age: "24",
-});
-// count scan
-var count_result = client.get("Standard", "todd", {limit: 1});
-assert.deepEqual(count_result, {
-  age: "24",
-});
-// range scan
-var range_result = client.get("Standard", "todd", {start: "", finish: "age"});
-assert.deepEqual(range_result, {
-  age: "24",
-});
-// reversed
-var reversed_result = client.get("Standard", "todd", {reversed: true, limit: 2});
-assert.deepEqual(reversed_result, {
-  id: "1",
-  last_name: "Dahl",
-});
-// modifying consistency level not yet supported
-/*
-client.insert("Standard", "jesse", {
-  id: 2,
-  first_name: "Jesse",
-  last_name: "Pitman"
-}, {
-  consistency_level: CL.ALL
-});
-*/
+    assert.deepEqual(selected_result_with_cl_any, {
+    id: '1',
+    age: '24',
+    });
+    */
+    // count scan
+    standard.get('todd', {count: 1}, function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        age: '24',
+      });
+    });
+    // range scan
+    standard.get('todd', {start: '', finish: 'age'}, function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        age: '24',
+      });
+    });
+    // modifying consistency level not yet supported
+    //client.set('Standard', 'jesse', {
+    //  id: 2,
+    //  first_name: 'Jesse',
+    //  last_name: 'Pitman'
+    //}, {
+    //  consistency_level: CL.ALL
+    //});
 
-// TTL is not supported yet.
-/*
-client.insert("Standard", "foo", {
-  bar: "baz"
-}, {
-  ttl: 1000
-});
-*/
+    // TTL is not supported yet.
+    //client.set('Standard', 'foo', {
+    //  bar: 'baz'
+    //}, {
+    //  ttl: 1000
+    //});
 
-// counting
-// let's count number of cols
-var count_result = client.count("Standard", "todd");
-assert.equal(4, count_result);
+    // counting
+    // let's count number of cols
+    standard.count('todd', function(err, res) {
+      assert.equal(err, null);
+      assert.equal(4, res);
+    });
 
-// you can count colmns of multiple keys
-var multicount_result = client.multicount("Standard", ["todd", "jesse"]);
-assert.deepEqual(multicount_result, {
-  todd: 4,
-  jesse: 3
-});
+    // you can count colmns of multiple keys
+    standard.count(['todd', 'jesse'], function(err, res){
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        todd: 4,
+        jesse: 3
+      });
+    });
 
-// super column
-client.insert("Super", "edgar", {
-  name:
-    {first_name: "Edgar", last_name: "Sawyers"},
-  address:
-    {city: "Madison", state: "WI"}
-});
+    // super column
+    superCF.set('edgar', {
+      name:
+    {first_name: 'Edgar', last_name: 'Sawyers'},
+      address:
+    {city: 'Madison', state: 'WI'}
+    }, function(err) {
+      assert.equal(err, null);
+    });
 
-var sc_data = client.get("Super", "edgar");
-assert.deepEqual(sc_data, {
-  name:
-    {first_name: "Edgar", last_name: "Sawyers"},
-  address:
-    {city: "Madison", state: "WI"}
-});
+    superCF.get('edgar', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        name:
+      {first_name: 'Edgar', last_name: 'Sawyers'},
+        address:
+      {city: 'Madison', state: 'WI'}
+      });
+    });
 
-sc_data = client.get("Super", "edgar", "address");
-assert.deepEqual(sc_data, {
-  city: "Madison",
-  state: "WI"
-});
+    superCF.get('edgar', 'address', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        city: 'Madison',
+        state: 'WI'
+      });
+    });
 
-sc_data = client.get("Super", "edgar", "address", ["city"]);
-assert.deepEqual(sc_data, {
-  city: "Madison"
-});
+    // get only one column for certain key
+    superCF.get('edgar', 'address', ['city'], function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        city: 'Madison'
+      });
+    });
+    // TODO clarify
+    superCF.get('edgar', 'address', {reversed: true, limit: 1}, function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        city: 'Madison',
+        state: 'WI'
+      });
+    });
 
-sc_data = client.get("Super", "edgar", "address", {reversed: true, limit: 1});
-assert.deepEqual(sc_data, {
-  state: "WI"
-});
+    // remove
+    standard.remove('todd', 'id', function(err) {
+      assert.equal(err, null);
+    });
+    standard.get('todd', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        first_name: 'Todd',
+        last_name: 'Dahl',
+        age: '24',
+      });
+    });
 
-// remove
-setTimeout(function() {
-  client.remove("Standard", "todd");
-  var removed = client.get("Standard", "todd");
-  assert.deepEqual(removed, {});
+    standard.remove('todd', ['first_name', 'last_name']);
+    standard.get('todd', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        age: '24',
+      });
+    });
 
-  client.remove("Standard", "jesse");
-  removed = client.get("Standard", "jesse");
-  assert.deepEqual(removed, {});
+    standard.remove('todd');
+    standard.get('todd', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {});
+    });
 
-  client.remove("Super", "edgar");
-  removed = client.get("Super", "edgar");
-  assert.deepEqual(removed, {});
-}, 100);
+    standard.remove('jesse');
+    standard.get('jesse', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {});
+    });
 
-console.log("tests are ok");
+    superCF.remove('edgar');
+    superCF.get('edgar', function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {});
+    });
+
+    // close connection before exit
+    beforeExit(function() { client.close(); });
+  }
+};
